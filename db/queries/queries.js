2c6c69e03bdcb0923f4564c8c9378f477482550b
Jason Melnikel <jmelnikel@gmail.com>
2020-04-04 17:10:03 +0000

    added FXN to get one menu item by id

--- db/queries/queries.js
+++ db/queries/queries.js
@@ -8,7 +8,8 @@ const pool = new Pool({
 });
 
 // ----menu_items----
-// This FXN will return all information about all menu items as an object
+// This FXN will return an object of all information about all
+// menu items.
 const getAllMenuItems = function() {
   const queryString = `
   SELECT *
@@ -19,5 +20,21 @@ const getAllMenuItems = function() {
   .then((res) => {
     return res.rows;
   }).catch(err => console.error('query error', err.stack));
-}
+};
 exports.getAllMenuItems = getAllMenuItems;
+
+// This FXN will return an object of all information
+// of one menu item taken in as argument as the menu_item id.
+const getOneMenuItem = function(id) {
+  const queryString = `
+  SELECT *
+  FROM menu_items
+  WHERE id = $1;
+  `;
+  const values = [id]
+  return pool.query(queryString, values)
+  .then ((res) => {
+    return res.rows[0];
+  }).catch(err => console.error('query error', err.stack));
+};
+exports.getOneMenuItem = getOneMenuItem;

27b9a69bab1f65c849a115c4103f153a33b34664
Taha Rizvi <riztaha@gmail.com>
2020-04-04 04:46:11 +0000

    Created views&routes for index,cart,menu,orders, & rest-menu

--- routes/users.js
+++ routes/users.js
@@ -5,20 +5,19 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const app = express();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
-      .then(data => {
+      .then((data) => {
         const users = data.rows;
         res.json({ users });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
   return router;

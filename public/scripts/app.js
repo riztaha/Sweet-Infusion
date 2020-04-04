30cb447cb554f6a62c2bf5aa29639fd9cf4dfc43
Taha Rizvi <TahaRizvi@Tahas-MacBook-Pro.local>
2020-04-04 19:51:50 -0400

    Removed /api/users

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,23 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users",
-  }).done((users) => {
-    for (user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });
-});
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users",
+//   }).done((users) => {
+//     for (user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });
+// });
 
 //API for Menu Items
 $(() => {
   $.ajax({
     method: "GET",
     url: "/api/menu",
-  }).done((menu) => {
-    for (item of menu) {
-      $("<div>").text(item).appendTo($("body"));
-    }
   });
+  // .done((menu) => {
+  //   for (let item of menu) {
+  //     $("<div>").text(item).appendTo($("body"));
+  //   }
+  // });
 });

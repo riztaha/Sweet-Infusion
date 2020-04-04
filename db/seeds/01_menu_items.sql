17b85bedc0bb334544d7bc155fbbb015fa533856
Jason Melnikel <jmelnikel@gmail.com>
2020-04-04 16:15:50 +0000

    stuff

--- db/seeds/01_menu_items.sql
+++ db/seeds/01_menu_items.sql
@@ -16,11 +16,11 @@ VALUES ('Confetti Cake', 750, 3, 'cakes', 'Lollipop candy canes chocolate cake i
 
 -- Bacon
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Yummy Bacon', 1000, 5, 'bacon', 'Bacon ipsum dolor amet tri-tip t-bone swine capicola spare ribs sirloin rump picanha frankfurter biltong drumstick meatloaf.', 'https://thebakermama.com/wp-content/uploads/2018/08/fullsizeoutput_15a7c.jpg');
+VALUES ('Yummy Bacon', 1000, 5, 'bacon', 'Bacon ipsum dolor amet tri-tip t-bone swine capicola spare ribs sirloin rump picanha frankfurter biltong drumstick meatloaf.', 'absent photo URL');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Chocolate Bacon', 1200, 7, 'bacon', 'Ribeye ground round jowl, ham hock strip steak fatback pancetta pork belly meatloaf. Chislic shank picanha fatback burgdoggen, ham hock rump meatloaf strip steak.', 'https://www.thespruceeats.com/thmb/rCmqbReIi6G4xU67Ks_CnN15CKc=/1500x1125/smart/filters:no_upscale()/chocolate-covered-bacon-58ae09ac3df78c345b2af207.jpg');
+VALUES ('Chocolate Bacon', 1200, 7, 'bacon', 'Ribeye ground round jowl, ham hock strip steak fatback pancetta pork belly meatloaf. Chislic shank picanha fatback burgdoggen, ham hock rump meatloaf strip steak.', 'absent photo URL');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Bacon Chips', 800, 5, 'bacon', 'Filet mignon andouille bresaola, ball tip short loin shank sausage biltong chislic salami ground round tail kevin alcatra.', 'https://healthyrecipesblogs.com/wp-content/uploads/2019/01/bacon-chips-square.jpg');
+VALUES ('Bacon Chips', 800, 5, 'bacon', 'Filet mignon andouille bresaola, ball tip short loin shank sausage biltong chislic salami ground round tail kevin alcatra.', 'absent photo URL');
 
 
 -- Milkshakes
@@ -33,8 +33,8 @@ VALUES ('Vanilla Milkshake', 350, 3, 'milkshakes', 'Boring vanilla. Enough said.
 
 -- Coffees
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Deluxe coffee', 350, 4, 'coffees', 'Java chicory, black doppio and roast cream mocha turkish strong. Blue mountain doppio black, chicory, sugar medium, single shot a wings blue mountain turkish.', 'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG');
+VALUES ('Deluxe coffee', 350, 4, 'coffees', 'Java chicory, black doppio and roast cream mocha turkish strong. Blue mountain doppio black, chicory, sugar medium, single shot a wings blue mountain turkish.', 'absent photo URL');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
 VALUES ('Electric coffee', 400, 5, 'coffees', 'Viennese et, cinnamon, turkish lungo qui cappuccino kopi-luwak. Black, dripper, to go medium espresso lungo in, and plunger pot latte sweet redeye.', '');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Cafe au Lait', 300, 3, 'coffees', 'Barista et bar aftertaste, brewed variety filter turkish breve organic. Mug french press rich doppio barista redeye cream.', 'https://media3.s-nbcnews.com/j/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p_67dfb6820f7d3898b5486975903c2e51.fit-760w.jpg');
+VALUES ('Cafe au Lait', 300, 3, 'coffees', 'Barista et bar aftertaste, brewed variety filter turkish breve organic. Mug french press rich doppio barista redeye cream.', 'absent photo URL');

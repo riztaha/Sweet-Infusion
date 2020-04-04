d80960c726470150b6e11222adb61fb797bb9185
Jason Melnikel <jmelnikel@gmail.com>
2020-04-04 20:19:12 +0000

    Fixed an error in customers table

--- db/seeds/02_customers.sql
+++ db/seeds/02_customers.sql
@@ -1,10 +1,10 @@
 INSERT INTO customers (first_name, last_name, email, phone, street, city, province, country, postal_code, credit_card, credit_card_exp)
-VALUES ('Tony', 'Stark', 't.stark@gmail.com', 416-876-3421, '532 Iron Rd.', 'Toronto', 'Ontario', 'M5W 3X2', '8437 5462 9068 4637', '05/21');
+VALUES ('Tony', 'Stark', 't.stark@gmail.com', 416-876-3421, '532 Iron Rd.', 'Toronto', 'Ontario', 'Canada', 'M5W 3X2', '8437 5462 9068 4637', '05/21');
 INSERT INTO customers (first_name, last_name, email, phone, street, city, province, country, postal_code, credit_card, credit_card_exp)
-VALUES ('Bruce', 'Banner', 'b.banner@gmail.com', 416-534-7534, '342 Hulk Ave.', 'Toronto', 'Ontario', 'M7K 4E2', '9586 8432 8574 6712', '12/22');
+VALUES ('Bruce', 'Banner', 'b.banner@gmail.com', 416-534-7534, '342 Hulk Ave.', 'Toronto', 'Ontario', 'Canada', 'M7K 4E2', '9586 8432 8574 6712', '12/22');
 INSERT INTO customers (first_name, last_name, email, phone, street, city, province, country, postal_code, credit_card, credit_card_exp)
-VALUES ('Steven', 'Rogers', 's.rogers@gmail.com', 416-321-5390, ' 897 Captain St.', 'Toronto', 'Ontario', 'M3R 7Q2', '3234 5641 7809 5436', '02/22');
+VALUES ('Steven', 'Rogers', 's.rogers@gmail.com', 416-321-5390, ' 897 Captain St.', 'Toronto', 'Ontario', 'Canada', 'M3R 7Q2', '3234 5641 7809 5436', '02/22');
 INSERT INTO customers (first_name, last_name, email, phone, street, city, province, country, postal_code, credit_card, credit_card_exp)
-VALUES ('Natasha', 'Romanoff', 'n.romanoff@gmail.com', 416-342-6512, '234 Widow Lane', 'Toronto', 'Ontario', 'M1R 6Y3', '5890 8743 8432 4532', '11/20');
+VALUES ('Natasha', 'Romanoff', 'n.romanoff@gmail.com', 416-342-6512, '234 Widow Lane', 'Toronto', 'Ontario', 'Canada', 'M1R 6Y3', '5890 8743 8432 4532', '11/20');
 INSERT INTO customers (first_name, last_name, email, phone, street, city, province, country, postal_code, credit_card, credit_card_exp)
-VALUES ('Clinton', 'Barton', 'c.barton@gmail.com', 416-541-7809, '908 Hawkeye St.', 'Toronto', 'Ontario', 'M4M 2V7', '3245 9087 4324 6510', '08/22');
+VALUES ('Clinton', 'Barton', 'c.barton@gmail.com', 416-541-7809, '908 Hawkeye St.', 'Toronto', 'Ontario', 'Canada', 'M4M 2V7', '3245 9087 4324 6510', '08/22');

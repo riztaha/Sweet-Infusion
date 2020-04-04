f9d31705ac700ba2ca94a9d5de421daaadd512c8
Jason Melnikel <jmelnikel@gmail.com>
2020-04-04 15:50:50 +0000

    add some picture URLs to the seeds

--- db/seeds/01_menu_items.sql
+++ db/seeds/01_menu_items.sql
@@ -1,40 +1,40 @@
 -- Pies
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Cherry Pie', 500, 2, "pies", "Chocolate lollipop halvah pie sweet roll gummi bears caramels. Tiramisu pastry wafer jujubes dessert gingerbread gummi bears chocolate candy canes.", "absent photo URL");
+VALUES ('Cherry Pie', 500, 2, 'pies', 'Chocolate lollipop halvah pie sweet roll gummi bears caramels. Tiramisu pastry wafer jujubes dessert gingerbread gummi bears chocolate candy canes.', 'absent photo URL');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Chocolate Pie', 600, 3, "pies", "Chocolate cake tiramisu marshmallow. Candy tart sugar plum.", "absent photo URL");
+VALUES ('Chocolate Pie', 600, 3, 'pies', 'Chocolate cake tiramisu marshmallow. Candy tart sugar plum.', 'absent photo URL');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Apple Pie', 650, 2, "pies", "Liquorice pastry cotton candy lemon drops cake ice cream sugar plum. Chocolate cake tiramisu cupcake tart oat cake candy canes halvah liquorice sweet roll.", "absent photo URL");
+VALUES ('Apple Pie', 650, 2, 'pies', 'Liquorice pastry cotton candy lemon drops cake ice cream sugar plum. Chocolate cake tiramisu cupcake tart oat cake candy canes halvah liquorice sweet roll.', 'absent photo URL');
 
 -- Cakes
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Red Velvet Cake', 850, 2, "cakes", "Tart cupcake gingerbread gummies sugar plum chocolate bar bear claw tootsie roll. Muffin danish cheesecake liquorice tiramisu.", "absent photo URL");
+VALUES ('Red Velvet Cake', 850, 2, 'cakes', 'Tart cupcake gingerbread gummies sugar plum chocolate bar bear claw tootsie roll. Muffin danish cheesecake liquorice tiramisu.', 'absent photo URL');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Black Forest Cake', 800, 3, "cakes", "Powder sweet tart pastry powder liquorice. Sugar plum sugar plum ice cream apple pie bear claw.", "absent photo URL");
+VALUES ('Black Forest Cake', 800, 3, 'cakes', 'Powder sweet tart pastry powder liquorice. Sugar plum sugar plum ice cream apple pie bear claw.', 'absent photo URL');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Confetti Cake', 750, 3, "cakes", "Lollipop candy canes chocolate cake icing cookie candy pie danish. Cupcake soufflé gingerbread tootsie roll candy canes powder.", "absent photo URL");
+VALUES ('Confetti Cake', 750, 3, 'cakes', 'Lollipop candy canes chocolate cake icing cookie candy pie danish. Cupcake soufflé gingerbread tootsie roll candy canes powder.', 'absent photo URL');
 
 -- Bacon
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Yummy Bacon', 1000, 5, "bacon", "Bacon ipsum dolor amet tri-tip t-bone swine capicola spare ribs sirloin rump picanha frankfurter biltong drumstick meatloaf.", "absent photo URL");
+VALUES ('Yummy Bacon', 1000, 5, 'bacon', 'Bacon ipsum dolor amet tri-tip t-bone swine capicola spare ribs sirloin rump picanha frankfurter biltong drumstick meatloaf.', 'https://thebakermama.com/wp-content/uploads/2018/08/fullsizeoutput_15a7c.jpg');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Chocolate Bacon', 1200, 7, "bacon", "Ribeye ground round jowl, ham hock strip steak fatback pancetta pork belly meatloaf. Chislic shank picanha fatback burgdoggen, ham hock rump meatloaf strip steak.", "absent photo URL");
+VALUES ('Chocolate Bacon', 1200, 7, 'bacon', 'Ribeye ground round jowl, ham hock strip steak fatback pancetta pork belly meatloaf. Chislic shank picanha fatback burgdoggen, ham hock rump meatloaf strip steak.', 'https://www.thespruceeats.com/thmb/rCmqbReIi6G4xU67Ks_CnN15CKc=/1500x1125/smart/filters:no_upscale()/chocolate-covered-bacon-58ae09ac3df78c345b2af207.jpg');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Bacon Chips', 800, 5, "bacon", "Filet mignon andouille bresaola, ball tip short loin shank sausage biltong chislic salami ground round tail kevin alcatra.", "absent photo URL");
+VALUES ('Bacon Chips', 800, 5, 'bacon', 'Filet mignon andouille bresaola, ball tip short loin shank sausage biltong chislic salami ground round tail kevin alcatra.', 'https://healthyrecipesblogs.com/wp-content/uploads/2019/01/bacon-chips-square.jpg');
 
 
 -- Milkshakes
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Strawberry Milkshake', 450, 4, "milkshakes", "Bloody mary glogg the blenheim saketini ectoplasm dom chivas regal.", "absent photo URL");
+VALUES ('Strawberry Milkshake', 450, 4, 'milkshakes', 'Bloody mary glogg the blenheim saketini ectoplasm dom chivas regal.', 'absent photo URL');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Grasshopper Milkshake', 550, 5, "milkshakes", "Manhattan irish coffee b & b bombay. Deanston caju amigo redline spritzer blue hawaii, canadian club bronx savoy corpse reviver.", "absent photo URL");
+VALUES ('Grasshopper Milkshake', 550, 5, 'milkshakes', 'Manhattan irish coffee b & b bombay. Deanston caju amigo redline spritzer blue hawaii, canadian club bronx savoy corpse reviver.', 'absent photo URL');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Vanilla Milkshake', 350, 3, "milkshakes", "Boring vanilla. Enough said.", "absent photo URL");
+VALUES ('Vanilla Milkshake', 350, 3, 'milkshakes', 'Boring vanilla. Enough said.', 'absent photo URL');
 
 -- Coffees
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Deluxe coffee', 350, 4, "coffees", "Java chicory, black doppio and roast cream mocha turkish strong. Blue mountain doppio black, chicory, sugar medium, single shot a wings blue mountain turkish.", "absent photo URL");
+VALUES ('Deluxe coffee', 350, 4, 'coffees', 'Java chicory, black doppio and roast cream mocha turkish strong. Blue mountain doppio black, chicory, sugar medium, single shot a wings blue mountain turkish.', 'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Electric coffee', 400, 5, "coffees", "Viennese et, cinnamon, turkish lungo qui cappuccino kopi-luwak. Black, dripper, to go medium espresso lungo in, and plunger pot latte sweet redeye.", "absent photo URL");
+VALUES ('Electric coffee', 400, 5, 'coffees', 'Viennese et, cinnamon, turkish lungo qui cappuccino kopi-luwak. Black, dripper, to go medium espresso lungo in, and plunger pot latte sweet redeye.', '');
 INSERT INTO menu_items (name, price, prep_time, category, description, photo)
-VALUES ('Cafe au Lait', 300, 3, "coffees", "Barista et bar aftertaste, brewed variety filter turkish breve organic. Mug french press rich doppio barista redeye cream.", "absent photo URL");
+VALUES ('Cafe au Lait', 300, 3, 'coffees', 'Barista et bar aftertaste, brewed variety filter turkish breve organic. Mug french press rich doppio barista redeye cream.', 'https://media3.s-nbcnews.com/j/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p_67dfb6820f7d3898b5486975903c2e51.fit-760w.jpg');

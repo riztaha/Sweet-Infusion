a0968bf8dc3535822d116926576c408f0059607c
Nazia Shaik <42496130+shaiknazia@users.noreply.github.com>
2020-04-06 14:51:23 -0400

    CSS files added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,39 +1,10 @@
-<<<<<<< HEAD
-
-//API for Menu Items
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/menu",
-  }).done((menu) => {
-    for (item of menu) {
-      $("<div>").text(item).appendTo($("body"));
+    url: "/api/users"
+  }).done((users) => {
+    for(user of users) {
+      $("<div>").text(user.name).appendTo($("body"));
     }
-  });
+  });;
 });
-
-=======
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users",
-//   }).done((users) => {
-//     for (user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });
-// });
-
-//API for Menu Items
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/menu",
-//   });
-// .done((menu) => {
-//   for (let item of menu) {
-//     $("<div>").text(item).appendTo($("body"));
-//   }
-// });
-// });
->>>>>>> 6386876b18d6aa9a1ae402c5e097d20adf5b9fa8

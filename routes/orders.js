1896208727072127411f295b5301011543fc0ead
Taha Rizvi <riztaha@gmail.com>
2020-04-07 18:45:39 +0000

    added db routes for orders

---
+++ routes/orders.js
@@ -0,0 +1,47 @@
+// const express = require("express");
+const queries = require("../db/queries/queries");
+
+const getOrders = (db) => {
+  const promise = new Promise((resolve, reject) => {
+    console.log("in getOrders function");
+    //Grabbing the function from queries.js
+    queries
+      .getOrders()
+      // db.query(queryString)
+      .then((data) => {
+        console.log("in data getOrders");
+        console.log(data);
+        // res.json({ menu_items });
+        resolve(data);
+      })
+      .catch((err) => {
+        console.error("query error", err.stack);
+        reject(err.stack);
+      });
+  });
+  return promise;
+};
+exports.getOrders = getOrders;
+
+// Function to get a specific customer's order
+const getCustomerOrder = function (db) {
+  const promise = new Promise((resolve, reject) => {
+    // console.log("in getAllMenuItems");
+    //Grabbing the function from queries.js
+    queries
+      .getCustomerOrder()
+      // db.query(queryString)
+      .then((data) => {
+        // console.log("in getAllMenuItems");
+        console.log(data);
+        // res.json({ menu_items });
+        resolve(data);
+      })
+      .catch((err) => {
+        console.error("query error", err.stack);
+        reject(err.stack);
+      });
+  });
+  return promise;
+};
+exports.getCustomerOrder = getCustomerOrder;

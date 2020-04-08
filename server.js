1ff6dd213b5ca9ae480b14fd429896015dec9903
Taha Rizvi <riztaha@gmail.com>
2020-04-08 19:36:01 +0000

    Trying to add/retrieve info to db for customer orders.

--- server.js
+++ server.js
@@ -45,12 +45,12 @@ const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
 const menuRoutes = require("./routes/menu");
 const orderRoutes = require("./routes/orders");
+const customerRouters = require("./routes/customers");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
-
 
 // Note: mount other resources here, using the same pattern above
 
@@ -93,6 +93,7 @@ app.get("/cart", (req, res) => {
 app.post("/cart", function (req, res) {
   console.log("CART ITEMS -------->", req.body);
   // Create customer table, with empty stuff
+
   // console.log(order)
   // orderRoutes.placeOrder(order)
   // Create new order with the menu items at the same time
@@ -107,9 +108,10 @@ app.get("/restaurant", function (req, res) {
 
 app.get("/complete", function (req, res) {
   // Show customer's info
+
   // Show order info
   orderRoutes
-    .getOrders(db, req["customer_id"])
+    .getCustomerOrder(db, req["customer_id"])
     .then((obj) => {
       res.render("complete", { orders: obj });
     })
@@ -120,10 +122,10 @@ app.get("/complete", function (req, res) {
 
 app.post("/complete", function (req, res) {
   let time = 0;
-  let phone = `+1${req.body.x_prom.split('-').join('')}`;
+  let phone = `+1${req.body.phone.split("-").join("")}`;
   // sendCustomerSMSText(phone, time) // this calls function to send text with phone, time
   // as argument to customer
-  let order = ''
+  let order = "";
   // sendRestaurantSMSText(order) // this calls function to send text to with order as argument
   // to restaurant.
   console.log("CREDIT CARD CUSTOMER INFO --------> ", req.body);
@@ -171,9 +173,6 @@ app.post("/complete", function (req, res) {
 //       to: '+14165353345'
 //     }).then(message => console.log(message.sid));
 // };
-
-
-
 
 //Server is listening.
 app.listen(PORT, () => {

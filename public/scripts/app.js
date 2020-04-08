c5365cd771fd7e0e31bcd8ccc8ceaf7ee21da475
Nazia Shaik <shaiknazia.n@gmail.com>
2020-04-08 14:40:38 +0000

    test

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,15 @@
 
 // // document ready
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/menu"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
+$(() => {
+  $.ajax({
+    method: "GET",
+    url: "/api/menu"
+  }).done((users) => {
+    for(user of users) {
+      $("<div>").text(user.name).appendTo($("body"));
+    }
+  });;
+});
 
 
 

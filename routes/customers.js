c86cd6fbc0bdfc861454a7cbc3a8e8b2e703a57e
Taha Rizvi <TahaRizvi@Tahas-MacBook-Pro.local>
2020-04-09 22:18:18 -0400

    Don't need to create empty customer anymore

--- routes/customers.js
+++ routes/customers.js
@@ -22,24 +22,24 @@ const getCustomers = function () {
 };
 exports.getCustomers = getCustomers;
 
-// Function to place customer's information into db
-const createEmptyCustomer = function () {
-  const promise = new Promise((resolve, reject) => {
-    console.log("in createEmptyCustomer");
-    //Grabbing the function from queries.js
-    queries
-      .createEmptyCustomer()
-      .then((data) => {
-        resolve(data);
-      })
-      .catch((err) => {
-        console.log("Promise error", err.stack);
-        reject(err.stack);
-      });
-  });
-  return promise;
-};
-exports.createEmptyCustomer = createEmptyCustomer;
+// // Function to place customer's information into db
+// const createEmptyCustomer = function () {
+//   const promise = new Promise((resolve, reject) => {
+//     console.log("in createEmptyCustomer");
+//     //Grabbing the function from queries.js
+//     queries
+//       .createEmptyCustomer()
+//       .then((data) => {
+//         resolve(data);
+//       })
+//       .catch((err) => {
+//         console.log("Promise error", err.stack);
+//         reject(err.stack);
+//       });
+//   });
+//   return promise;
+// };
+// exports.createEmptyCustomer = createEmptyCustomer;
 
 // Function to place customer's information into db
 const placeCustomerInfo = function (customer) {

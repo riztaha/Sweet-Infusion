2893142cb7ee1eb2bdea5762344f371bf61bf048
Taha Rizvi <TahaRizvi@Tahas-MacBook-Pro.local>
2020-04-10 07:07:24 -0400

    Added code for 4 new queries.

--- db/queries/queries.js
+++ db/queries/queries.js
@@ -16,11 +16,6 @@ const getAllMenuItems = function () {
   FROM menu_items;
   `;
   const values = [];
-  //Jason, return a promise here, don't return the pool. Once the promise
-  //gets resolved, it is then used in the function in menu.js where
-  //that function is used in server.js and renders it
-  //as a tempvar. This needs to be a promise because it needs to resolve from the database
-  //before it can show up in the index page.
   const promise = new Promise((resolve, reject) => {
     pool
       .query(queryString, values)
@@ -36,71 +31,111 @@ const getAllMenuItems = function () {
 };
 exports.getAllMenuItems = getAllMenuItems;
 
-// This FXN will return an object of all information
-// of one menu item taken in as argument as the menu_item id.
-const getOneMenuItem = function (id) {
-  const queryString = `
-  SELECT *
-  FROM menu_items
-  WHERE id = $1;
-  `;
-  const values = [id];
-  return pool
-    .query(queryString, values)
-    .then((res) => {
-      return res.rows[0];
-    })
-    .catch((err) => console.log("get one Menu item query error", err.stack));
-};
-exports.getOneMenuItem = getOneMenuItem;
+// // This FXN will return an object of all information
+// // of one menu item taken in as argument as the menu_item id.
+// const getOneMenuItem = function (id) {
+//   const queryString = `
+//   SELECT *
+//   FROM menu_items
+//   WHERE id = $1;
+//   `;
+//   const values = [id];
+//   return pool
+//     .query(queryString, values)
+//     .then((res) => {
+//       return res.rows[0];
+//     })
+//     .catch((err) => console.log("get one Menu item query error", err.stack));
+// };
+// exports.getOneMenuItem = getOneMenuItem;
 
 // Function to Get all orders
-const getOrders = function () {
+// const getOrders = function () {
+//   const queryString = `
+//   SELECT *
+//   FROM orders;
+//   `;
+//   return pool
+//     .query(queryString)
+//     .then((res) => {
+//       return res.rows;
+//     })
+//     .catch((err) => console.log("get orders query error", err.stack));
+// };
+// exports.getOrders = getOrders;
+
+const setCustomerOrderComplete = function (order_id) {
   const queryString = `
-  SELECT *
-  FROM orders;
+  UPDATE orders
+  SET is_order_complete = true WHERE id = $1;
   `;
+  // console.log(order_id);
+  const queryParams = [order_id];
+  return pool
+    .query(queryString, queryParams)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) =>
+      console.log("complete customer order query error", err.stack)
+    );
+};
+exports.setCustomerOrderComplete = setCustomerOrderComplete;
+
+//Get's a customer's phone number by their order_id
+const getCustomerPhone = function (order_id) {
+  const queryString = `
+  SELECT phone
+  FROM customers
+  JOIN orders ON customer_id = customers.id
+  WHERE orders.id = $1;
+  `;
+  // console.log(order_id);
+  const queryParams = [order_id];
+  return pool
+    .query(queryString, queryParams)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) =>
+      console.log("complete customer order query error", err.stack)
+    );
+};
+exports.getCustomerPhone = getCustomerPhone;
+
+// // Function to get all pending menu items / orders
+const getPendingOrders = function () {
+  const queryString = `
+  SELECT order_id, customers.name as customer_name, customers.phone as customer_phone,
+  menu_items.name as item, order_menu_item.item_quantity as quantity
+  FROM orders
+  JOIN order_menu_item ON order_menu_item.order_id = orders.id
+  JOIN menu_items ON order_menu_item.menu_item_id = menu_items.id JOIN customers ON customer_id = customers.id
+  WHERE is_order_complete = false
+  GROUP BY order_id, customers.name, customers.phone, item, quantity;`;
   return pool
     .query(queryString)
     .then((res) => {
       return res.rows;
     })
-    .catch((err) => console.log("get orders query error", err.stack));
+    .catch((err) => console.log("get pending orders query error", err.stack));
 };
-exports.getOrders = getOrders;
+exports.getPendingOrders = getPendingOrders;
 
-// Function to get a specific customer's order using the order_menu_item table
-const getCustomerOrder = function (order_id) {
-  const queryString = `
-  SELECT *
-  FROM order_menu_item
-  JOIN menu_items ON menu_item_id = menu_items.id
-  JOIN orders ON order_id = orders.id
-  WHERE order_id = $1;`;
-  const values = [order_id];
-  return pool
-    .query(queryString, values)
-    .then((res) => {
-      return res.rows;
-    })
-    .catch((err) => console.log("get customer order query error", err.stack));
-};
-exports.getCustomerOrder = getCustomerOrder;
-
-// Function to get all customers
-const getCustomers = function () {
-  const queryString = `
-  SELECT *
-  FROM customers;
-  `;
-  return pool
-    .query(queryString)
-    .then((res) => {
-      return res.rows;
-    })
-    .catch((err) => console.log("get customers query error", err.stack));
-};
-exports.getCustomers = getCustomers;
+// // Function to get all customers
+// const getCustomers = function () {
+//   const queryString = `
+//   SELECT *
+//   FROM customers;
+//   `;
+//   return pool
+//     .query(queryString)
+//     .then((res) => {
+//       return res.rows;
+//     })
+//     .catch((err) => console.log("get customers query error", err.stack));
+// };
+// exports.getCustomers = getCustomers;
 
 // Function to place an order
 const placeOrder = function (order) {
@@ -160,11 +195,6 @@ exports.createOrder = createOrder;
 
 // Function to edit a customer's information
 const placeCustomerInfo = function (customer) {
-  // const queryString = `
-  // UPDATE customers
-  // SET name = $1, phone = $2, address = $3, zip_code = $4, credit_card = $5, credit_card_exp = $6, credit_card_code = $7
-  // WHERE name = 'undefined';
-  // `;
   const queryString = `
   INSERT INTO customers
   (name, phone, address, zip_code, credit_card, credit_card_exp, credit_card_code)
@@ -190,19 +220,19 @@ const placeCustomerInfo = function (customer) {
 };
 exports.placeCustomerInfo = placeCustomerInfo;
 
-// Function to get all customers
-const getLastCustomer = function () {
-  const queryString = `
-  SELECT *
-  FROM customers
-  ORDER BY id
-  DESC LIMIT 1;
-  `;
-  return pool
-    .query(queryString)
-    .then((res) => {
-      return res.rows;
-    })
-    .catch((err) => console.log("get last customer query error", err.stack));
-};
-exports.getLastCustomer = getLastCustomer;
+// // Function to get all customers
+// const getLastCustomer = function () {
+//   const queryString = `
+//   SELECT *
+//   FROM customers
+//   ORDER BY id
+//   DESC LIMIT 1;
+//   `;
+//   return pool
+//     .query(queryString)
+//     .then((res) => {
+//       return res.rows;
+//     })
+//     .catch((err) => console.log("get last customer query error", err.stack));
+// };
+// exports.getLastCustomer = getLastCustomer;

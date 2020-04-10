4e1af49560d6b68cd5ec394b64576d17bda4497b
Taha Rizvi <TahaRizvi@Tahas-MacBook-Pro.local>
2020-04-10 07:08:35 -0400

    Added JQuery for restaurants view

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,9 @@
 // // // document ready
 $(() => {
-  // <!-- add button 'add to cart' with class name add -->
+  // <!-- add button 'add to cart' with
+  // ass name add -->
   let cart = [];
+  let orders = [];
 
   $(".add_to_cart").click(function () {
     let name = $(this).data("name");
@@ -102,4 +104,86 @@ $(() => {
     $("#subtotal").html(subtotal.toFixed(2));
     $("#total").html(total.toFixed(2));
   }
+
+  // JQUERY for restaurant-side
+
+  function groupItemsByOrderId() {
+    result = orders.reduce(function (r, a) {
+      r[a.order_id] = r[a.order_id] || [];
+      r[a.order_id].push(a);
+      return r;
+    }, Object.create(null));
+    // console.log(result);
+    return result;
+  }
+
+  function renderPendingOrders() {
+    $("#pending-orders").empty();
+    //If the object (viewed as an array) has keys in it, then
+    if (Object.keys(orders).length > 0) {
+      //Looping through the orders object
+      // let name = "";
+      // let phone = "";
+      // const unique = orders555.map((item) => {
+      //   // console.log("item 1 =====>", item[1]);
+      //   item[1].forEach((value) => {
+      //     name = value.customer_name;
+      //     phone = value.customer_phone;
+      //   });
+      // });
+
+      let pendingHtml = "";
+
+      // const orders555 = Object.entries(orders);
+      // orders555.map((item) => {
+      //   console.log("item  =====>", item[1]);
+      //   item[1].forEach((value) =>
+      //     // console.log("value", value.customer_name, value.customer_phone);
+      //     pendingHtml = `<h3> Customer Name: ${value.customer_name}, Customer Phone: ${value.customer_phone}`;
+      //   );
+      // });
+
+      for (let orderId in orders) {
+        pendingHtml += `<h3>Invoice ${orderId} for Customer Name:  Customer Number:  </h3>`;
+        //Looping through the orderID array
+        for (let item of orders[orderId]) {
+          pendingHtml += `<pre> ${item.item} - quantity ${item.quantity} </pre>`;
+        }
+        pendingHtml +=
+          `<input value='Complete Order' type='button' class='btn completeOrder' data-id=` +
+          orderId +
+          `><br>`;
+      }
+      $("#pending-orders").append(pendingHtml);
+    }
+  }
+
+  function markAsComplete(order_id) {
+    $.post("/completeOrder", { order_id }).done(function (data) {
+      console.log(data);
+      delete orders[order_id];
+      renderPendingOrders();
+    });
+  }
+
+  $("#pending-orders").on("click", ".completeOrder", function (event) {
+    console.log(event.data);
+    // event.preventDefault();
+    // console.log($(event.target))
+    let order_id = $(this).data("id");
+    markAsComplete(order_id);
+  });
+
+  function doPollForPendingOrders() {
+    $.get("/pendingOrders", function (data) {
+      orders = JSON.parse(data);
+      console.log("pending orders:");
+      // console.log("orders", orders); // process results here
+      orders = groupItemsByOrderId();
+      // console.log("Grouped items by order ID", orders); // process results here
+      renderPendingOrders();
+      setTimeout(doPollForPendingOrders, 30000); //Page will keep refreshing every 30s to see if any new orders have been added
+    });
+  }
+  doPollForPendingOrders();
 });
